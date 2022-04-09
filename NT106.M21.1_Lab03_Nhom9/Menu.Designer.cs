86070fd9e6bd53a9088171a9f5da4595b046c38b
Rabbithole07 <102034724+tranhunghoan@users.noreply.github.com>
2022-04-09 23:12:40 +0700

    Menu

--- NT106.M21.1_Lab03_Nhom9/Menu.Designer.cs
+++ NT106.M21.1_Lab03_Nhom9/Menu.Designer.cs
@@ -29,20 +29,120 @@ namespace NT106.M21._1_Lab03_Nhom9
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_Bai1 = new System.Windows.Forms.Button();
+            this.btn_Bai2 = new System.Windows.Forms.Button();
+            this.btn_Bai3 = new System.Windows.Forms.Button();
+            this.btn_Bai4 = new System.Windows.Forms.Button();
+            this.btn_Bai5 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lab 3 Menu";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_Bai5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Bai4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Bai3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Bai2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Bai1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(248, 106);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(254, 313);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btn_Bai1
+            // 
+            this.btn_Bai1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bai1.Location = new System.Drawing.Point(3, 3);
+            this.btn_Bai1.Name = "btn_Bai1";
+            this.btn_Bai1.Size = new System.Drawing.Size(248, 56);
+            this.btn_Bai1.TabIndex = 2;
+            this.btn_Bai1.Text = "Bai 1";
+            this.btn_Bai1.UseVisualStyleBackColor = true;
+            // 
+            // btn_Bai2
+            // 
+            this.btn_Bai2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bai2.Location = new System.Drawing.Point(3, 65);
+            this.btn_Bai2.Name = "btn_Bai2";
+            this.btn_Bai2.Size = new System.Drawing.Size(248, 56);
+            this.btn_Bai2.TabIndex = 3;
+            this.btn_Bai2.Text = "Bai 2";
+            this.btn_Bai2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Bai3
+            // 
+            this.btn_Bai3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bai3.Location = new System.Drawing.Point(3, 127);
+            this.btn_Bai3.Name = "btn_Bai3";
+            this.btn_Bai3.Size = new System.Drawing.Size(248, 56);
+            this.btn_Bai3.TabIndex = 4;
+            this.btn_Bai3.Text = "Bai 3";
+            this.btn_Bai3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Bai4
+            // 
+            this.btn_Bai4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bai4.Location = new System.Drawing.Point(3, 189);
+            this.btn_Bai4.Name = "btn_Bai4";
+            this.btn_Bai4.Size = new System.Drawing.Size(248, 56);
+            this.btn_Bai4.TabIndex = 5;
+            this.btn_Bai4.Text = "Bai 4";
+            this.btn_Bai4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Bai5
+            // 
+            this.btn_Bai5.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bai5.Location = new System.Drawing.Point(3, 251);
+            this.btn_Bai5.Name = "btn_Bai5";
+            this.btn_Bai5.Size = new System.Drawing.Size(248, 59);
+            this.btn_Bai5.TabIndex = 6;
+            this.btn_Bai5.Text = "Bai 5";
+            this.btn_Bai5.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
             this.Name = "Menu";
             this.Text = "Menu";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn_Bai5;
+        private System.Windows.Forms.Button btn_Bai4;
+        private System.Windows.Forms.Button btn_Bai3;
+        private System.Windows.Forms.Button btn_Bai2;
+        private System.Windows.Forms.Button btn_Bai1;
     }
 }
 
